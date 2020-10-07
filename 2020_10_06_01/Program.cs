70d25474b40526facc2c1afb4104d7586dc83338
GDC6 <GDC6@DESKTOP-B95NHLH>
2020-10-07 09:42:28 +0900

    초기 커밋.

---
+++ 2020_10_06_01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2020_10_06_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//#if TRACE
+//            string name = Console.ReadLine();
+//            string pwd = Console.ReadLine();
+//#endif
+#if DEBUG
+            string name = "홍길동";
+            string pwd = "1234";
+#endif
+            Trace.WriteLine(name);
+            Console.WriteLine(name);
+            Console.WriteLine(pwd);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2020_10_06_01/indexer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2020_10_06_01
+{
+    class IntegerText
+    {
+        char[] txtNumver;
+        public IntegerText(int number)
+        {
+            txtNumver = number.ToString().ToCharArray();
+        }
+
+        public char this[int index]
+        {
+            get => txtNumver[txtNumver.Length - index - 1];
+            set => txtNumver[txtNumver.Length - index - 1] = value;
+        }
+
+        public char GetPointNumber(int Point)
+        {
+            return txtNumver[txtNumver.Length - Point - 1];
+        }
+    }
+
+    class indexer
+    {
+        static void Main()
+        {
+            IntegerText c1 = new IntegerText(123456);
+            int step = 1;
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine($"{step}의 자릿수" + c1[i]);
+                step = step * 10;
+            }
+        }
+    }
+}
